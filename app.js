ac16c73b1c5001fc2a2ea8306c51073e5a9d5b90
ciyuxu <ciyuxu@gmail.com>
2019-08-01 22:58:11 +0200

    add check box and cross off when task is done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = new Vue({
   el: "#simpletasks",
   data: {
-    title: "Add and check tasks here",
+    title: "Tasks overview",
     tasks: [
       { text: "task 1", done: false, id: Date.now() },
       { text: "task 2", done: false, id: Date.now() + 1 }
