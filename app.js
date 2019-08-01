af24c818107eb0fc951841a5144758b2f131d0ee
ciyuxu <ciyuxu@gmail.com>
2019-08-01 23:39:24 +0200

    add new task to the task list

--- app.js
+++ app.js
@@ -2,10 +2,20 @@ var app = new Vue({
   el: "#simpletasks",
   data: {
     title: "Tasks overview",
+    newTask: "",
+
     tasks: [
       { text: "task 1", done: false, id: Date.now() },
       { text: "task 2", done: false, id: Date.now() + 1 }
     ]
   },
-  methods: {}
+  methods: {
+    addTask() {
+      this.tasks.push({
+        text: this.newTask,
+        done: false,
+        id: Date.now()
+      });
+    }
+  }
 });
