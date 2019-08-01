700114e1c91d94de980eab6bc1cbde7959829658
ciyuxu <ciyuxu@gmail.com>
2019-08-01 22:36:57 +0200

    display list of tasks

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var app = new Vue({
   el: "#simpletasks",
   data: {
-    content: "Add and check tasks here"
-  }
+    title: "Add and check tasks here",
+    tasks: [
+      { text: "task 1", done: false, id: Date.now() },
+      { text: "task 2", done: false, id: Date.now() + 1 }
+    ]
+  },
+  methods: {}
 });
